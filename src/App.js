7663ed8dc9e4df63483d8db0aaa6a54876d8d9f2
etonar <anonymetonar@gmail.com>
2021-06-02 19:30:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
 
-// icons
-import { FaSearch } from 'react-icons/fa';
-
-// component
-import SingleItem from './single-image';
-
-//URLS
-const mainURL = 'https://api.unsplash.com/photos/';
-const searchURL = 'https://api.unsplash.com/search/photos/';
+import Photo from './Photo'
+const clientID = '?client_id=f7N-c7ynV9x6FAE3c1mP35-_1uRQeFNKMYlRro55XGA'
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [query, setQuery] = useState('');
-
-  const clientID = '?client_id=f7N-c7ynV9x6FAE3c1mP35-_1uRQeFNKMYlRro55XGA';
-
-  const getData = async () => {
-    setLoading(true);
-
-    let url;
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
+  const [query, setQuery] = useState('')
+  const fetchImages = async () => {
+    setLoading(true)
+    let url
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
     if (query) {
-      url = `${searchURL}${clientID}&page=${page}&query=${query}`;
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
     } else {
-      url = `${mainURL}${clientID}&page=${page}`;
+      url = `${mainUrl}${clientID}${urlPage}`
     }
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    setData((oldData) => {
-      if (query && page === 1) {
-        return data.results;
-      } else if (query) {
-        return [...oldData, ...data.results];
-      } else {
-        return [...oldData, ...data];
-      }
-    });
-    setLoading(false);
-  };
-
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results
+        } else if (query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
+      })
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
   useEffect(() => {
-    getData();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+    fetchImages()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page])
 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
@@ -54,52 +50,45 @@ function App() {
         (!loading && window.innerHeight + window.scrollY) >=
         document.body.scrollHeight - 2
       ) {
-       
-
         setPage((oldPage) => {
-          return oldPage + 1;
-        });
+          return oldPage + 1
+        })
       }
-    });
-    return () => window.removeEventListener('scroll', event);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const handlSubmit = (e) => {
-    e.preventDefault();
-    setPage(1);
-    getData();
-  };
-
+    })
+    return () => window.removeEventListener('scroll', event)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPage(() => 1)
+    fetchImages()
+  }
   return (
-    <div className="container">
-      <form
-        className="form"
-        onSubmit={(e) => {
-          handlSubmit(e);
-        }}
-      >
-        <div>
+    <main>
+      <section className='search'>
+        <form className='search-form'>
           <input
-            type="text"
-            placeholder="Search"
+            type='text'
+            placeholder='search'
             value={query}
-            onChange={(e) => {
-              setQuery(e.target.value);
-            }}
+            onChange={(e) => setQuery(e.target.value)}
+            className='form-input'
           />
-          <FaSearch />
-        </div>
-      </form>
-
-      <section className="images-container">
-        {data.map((item) => {
-          return <SingleItem key={item.id} {...item} />;
-        })}
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
       </section>
-      {loading && <h1 className="loading">loading...</h1>}
-    </div>
-  );
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
